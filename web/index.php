89c3f9d432b7f9e7342cc76e8d0645acc0a030f7
picanto <compot2004@yandex.ru>
2020-12-14 19:59:21 +0300

    Подключение simple_html_dom

--- web/index.php
+++ web/index.php
@@ -4,6 +4,7 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
+require  __DIR__ . '/../vendor/simple_html_dom/simple_html_dom.php';
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
