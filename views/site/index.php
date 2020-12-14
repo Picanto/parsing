eaf79e7cf4ee596acd53fed9c040609c33dfb527
picanto <compot2004@yandex.ru>
2020-12-14 21:08:27 +0300

    На главную страницу добавлено описание задания и ссылка на экшен Parser

--- views/site/index.php
+++ views/site/index.php
@@ -7,47 +7,31 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Парсинг https://gkb81.ru/sovety/</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://parsing/web/site/parser">Распарсить сайт</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+            <div class="col-lg-12">
+                <h2>Описание задания</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                Необходимо создать парсер на [yii2|Symfony] с сохранением в базу.<br>
+                Результат задания должен быть архив с файлами которые попадают под гит<br>
+                В качестве донора использовать https://gkb81.ru/sovety/<br>
+                В базу необходимо сохранить данные:<br>
+                1.Название статьи<br>
+                2.Ссылка на статьи<br>
+                3.Детальный текст статьи<br>
+                4.Дата публикации статьи<br>
+                5.Сохранить в папку картинку (превью)<br>
+                *Дополнительная задача (необязательная, если есть знание Битрикс)<br>
+                Написать под 1С-Битрикс агент, который будет каждые 60 минут активировать парсер<br>
             </div>
         </div>
 
     </div>
+
 </div>
